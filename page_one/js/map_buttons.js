6908bae6413f2ab337a17af3cd32b0e18ad43616
Lloyd Arnold <lloarn@gmail.com>
2020-06-04 11:32:41 +0100

    Move map loading to separate file bug free

--- page_one/js/map_buttons.js
+++ page_one/js/map_buttons.js
@@ -1,4 +1,12 @@
-function initMap() {
+// Create the script tag, set the appropriate attributes
+var script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBDyRTmTJDL3YvBF1cMr5BrEpDsYWSQ1Vg&callback=initMap';
+script.defer = true;
+script.async = true;
+
+// Attach your callback function to the `window` object
+window.initMap = function initMap(){
+  // JS API is loaded and available
   var takeaway = {lat: 50.750212, lng: -1.983003};
   var map = new google.maps.Map(
   document.getElementById('map'), {zoom: 15, center: takeaway});
@@ -6,10 +14,10 @@ function initMap() {
   var marker = new google.maps.Marker({position: takeaway, map: map});
 
   // key = AIzaSyBDyRTmTJDL3YvBF1cMr5BrEpDsYWSQ1Vg
-}
+};
 
-/* <!--Load the API from the specified URL
-* The async attribute allows the browser to render the page while the API loads
-* The key parameter will contain your own API key (which is not needed for this tutorial)
-* The callback parameter executes the initMap() function
---> */
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
+
+
+// getElementsByClassName('map-finder')
