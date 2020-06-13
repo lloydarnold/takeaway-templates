0102a1298e1a0c899c3b149960c65d057f524de9
Lloyd Arnold <lloarn@gmail.com>
2020-06-13 12:19:07 +0100

    Change js to cover left solumn when sidebar unfolded

--- page_four/js/sidebar_driver.js
+++ page_four/js/sidebar_driver.js
@@ -1,7 +1,10 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("container").style.marginLeft = "250px";
+  var temp;
+  temp = document.getElementById("left-column").scrollWidth;
+  temp = 250 - temp
+  document.getElementById("container").style.marginLeft = `${temp}px`;
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
